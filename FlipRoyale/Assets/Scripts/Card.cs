2c95b970871ed43ce5625665a9faa49c830d7eea
SukantPatra <sukantpatra070@gmail.com>
2026-02-19 22:38:08 +0530

    Card match - mismatch logic added

--- FlipRoyale/Assets/Scripts/Card.cs
+++ FlipRoyale/Assets/Scripts/Card.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,39 +14,76 @@ public class Card : MonoBehaviour, IPointerDownHandler
 
     private Image cardImage;
     private TweenManager tween;
+    private GameManager gameManager;
     private RectTransform rectTransform;
+    private bool isMatched = false;
+    private bool isShowing = false;
+    private bool readyToMatch = false;
+
+    [SerializeField] private int cardId;
 
     // Start is called before the first frame update
     void Start()
     {
         cardImage = gameObject.GetComponent<Image>();
         tween = TweenManager.Instance;
+        gameManager = GameManager.Instance;
         rectTransform = gameObject.GetComponent<RectTransform>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        if (isShowing || isMatched) return; //TODO: Add Game Over condition later
+
+        gameManager.OnCardSelected(this);
         StartCoroutine(ShowCardAnim());
     }
 
     private IEnumerator ShowCardAnim()
     {
-        tween.Scale(rectTransform, new Vector3(0,1,1), 0.25f);
-        yield return new WaitForSeconds(0.25f);
+        isShowing = true;
+        tween.Scale(rectTransform, new Vector3(0,1,1), 0.15f);
+        yield return new WaitForSeconds(0.15f);
         ShowCardFront();
-        tween.Scale(rectTransform, Vector3.one,0.25f);
+        tween.Scale(rectTransform, Vector3.one,0.15f);
+        readyToMatch = true;
+    }
 
-        yield return null;
+    public void CloseCard()
+    {
+        if (!isShowing || isMatched) return; // Prevent closing if the card is not showing
+        StartCoroutine(CloseCardCoroutine());
+    }
+    private IEnumerator CloseCardCoroutine()
+    {
+        tween.Scale(rectTransform, new Vector3(0,1,1), 0.15f);
+        yield return new WaitForSeconds(0.15f);
+
+        // Reset to cardBack image
+        ShowCardBack();
+        tween.Scale(rectTransform, Vector3.one, 0.15f);
+
+        isShowing = false;
+        readyToMatch = false;
+    }
+
+    public void DisableCard()
+    {
+        isMatched = true; //Safe check to ensure the card is not matched again
+        tween.SetOnComplete(tween.Scale(rectTransform, Vector3.zero, 0.15f), () => gameObject.SetActive(false));
     }
 
 
     public void ShowCardFront() => cardImage.sprite = cardSprite;
     public void ShowCardBack() => cardImage.sprite = cardBackSprite;
+    public int GetCardId() => cardId;
+    public void SetCardId(int _cardId) => cardId = _cardId;
+    public bool GetIsMatched() => isMatched;
+    public void SetIsMatched(bool _isMatched) => isMatched = _isMatched;
+    public bool GetIsShowing() => isShowing;
+    public void SetIsShowing(bool _isShowing) => isShowing = _isShowing;
+    public bool GetIsReadyToMatch() => readyToMatch;
+    public void SetIsReadyToMatch(bool _readyToMatch) => readyToMatch = _readyToMatch;
+
 
 }
--- FlipRoyale/Assets/Scripts/GameManager.cs
+++ FlipRoyale/Assets/Scripts/GameManager.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance { get; private set; }
+
+    private List<Card> currentPair = new List<Card>();
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnCardSelected(Card clickedCard)
     {
-        
+        currentPair.Add(clickedCard);
+
+        if (currentPair.Count >= 2)
+        {
+            // Copy the pair to local variables so further clicks won't interfere
+            Card first = currentPair[0];
+            Card second = currentPair[1];
+            currentPair = new List<Card>();
+
+            StartCoroutine(CheckMatch(first, second));
+        }
     }
+
+    private IEnumerator CheckMatch(Card first, Card second)
+    {
+
+        yield return new WaitUntil(() => first.GetIsReadyToMatch() && second.GetIsReadyToMatch());
+
+        if (first.GetCardId() == second.GetCardId())
+        {
+            //TODO: match SFX
+            //TODO: Increase score
+
+            first.SetIsMatched(true);
+            second.SetIsMatched(true);
+
+            yield return new WaitForSeconds(0.3f);
+            first.DisableCard();
+            second.DisableCard();
+        }
+        else
+        {
+            //TODO: mismatch SFX
+
+            yield return new WaitForSeconds(1f); //Adding a little delay to show the mismatch
+            first.CloseCard();
+            second.CloseCard();
+        }
+
+        yield return null;
+    }
+
+
 }
